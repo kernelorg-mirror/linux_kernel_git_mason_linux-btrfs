6eb23136f1d35452008e6a442e152cf89e2f1d4b
Chris Mason <clm@fb.com>
2019-05-06 06:29:17 -0700

    Btrfs: use wbc->for_cgroot in extent_write_locked_range()
    This is a helper function called while writing out compressed extents,
    and it needs to backcharge all of the io to the correct cgroup.
    
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -4055,6 +4055,8 @@ int extent_write_locked_range(struct inode *inode, u64 start, u64 end,
 		.nr_to_write	= nr_pages * 2,
 		.range_start	= start,
 		.range_end	= end + 1,
+		/* we're called from an async helper function */
+		.for_cgroot	= 1,
 	};
 
 	wbc_attach_fdatawrite_inode(&wbc_writepages, inode);
